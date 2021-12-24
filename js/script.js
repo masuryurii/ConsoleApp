7151edce9cf218605011ae35c23fa6adfef99002
Yurii <yuriimazur1@gmail.com>
2021-12-24 11:30:35 +0200

    Consile App of watched movies

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict"
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренныз фильмов?', '');
+const b = prompt('На сколько оцените его?', '');
+const c = prompt('Один из последних просмотренныз фильмов?', '');
+const d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
